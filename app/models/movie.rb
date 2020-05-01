15bee02a419e302e95d7b59db6f03fcc2dd8d4ed
patron <withyou01200524@gmail.com>
2020-05-01 07:37:48 +0000

    add some validate

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   validates :Title, presence: true
+  validates :Date, presence: true
+  validates :Theater, presence: true
   
   def self.search(search)
     return Movie.all unless search
